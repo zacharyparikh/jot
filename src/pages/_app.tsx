819cffdc0904f88b1c34aa223f7274aa1f8d90ad
Zachary Parikh <parikhzachary@gmail.com>
2022-09-24 23:05:14 -0400

    Add Redux. Change app structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
